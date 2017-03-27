0cac156829a36ee8c1878b9b726acf03ca00ae23
chris-jasztrab <chris.jasztrab@gmail.com>
2017-03-27 15:41:17 -0400

    Includes folder

---
+++ includes/layouts/footer.php
@@ -0,0 +1,5 @@
+<div id="footer">
+  Copyright 2017-2018, Milton Public Library
+</div>
+</body>
+</html>
---
+++ includes/layouts/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+<head>
+  <title>Battle of the Books</title>
+  <link rel="stylesheet" type="text/css" href="css/public.css" />
+</head>
+  <body>
+    <div id="header">
+      <h1>Battle of the Books</h1>
+    </div>
